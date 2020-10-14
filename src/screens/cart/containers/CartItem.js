854480c63ae27227e847dac4bf8e91d9177d9265
usman1579 <agentcj79@gmail.com>
2020-10-14 22:25:25 +0500

    Weighted Products price removed

--- src/screens/cart/containers/CartItem.js
+++ src/screens/cart/containers/CartItem.js
@@ -1,9 +1,9 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import split from 'lodash/split';
 import unescape from 'lodash/unescape';
 
-import { StyleSheet, View,Text } from 'react-native';
-import {  Image, ThemeConsumer } from 'src/components';
+import { StyleSheet, View, Text } from 'react-native';
+import { Image, ThemeConsumer } from 'src/components';
 import { Row, Col } from 'src/containers/Gird';
 import Quantity from 'src/containers/Quantity';
 
@@ -22,7 +22,10 @@ const getUrlImage = thumb => {
   return split(array ?.[1] ?? '', '"')[0];
 };
 
+
 function CartItem(props) {
+
+  const [Val, setVal] = useState(-1)
   const { item, currency, updateQuantity, goToProduct, style } = props;
   console.log('Item in variation', item)
   if (!item) {
@@ -38,7 +41,26 @@ function CartItem(props) {
     line_subtotal,
   } = item;
   const image = thumb || getUrlImage(thumbnail);
-  
+
+  const HideTotal = (line_subtotal, quantity, currency, item) => {
+    console.log('item variation length', item.variation.length)
+    if (item.variation.length == 0) {
+      return currencyFormatter(line_subtotal / quantity, currency)
+    } else {
+
+      Object.values(item.variation).map((data, index) => {
+        if (data.search('lb') > -1) {
+          setVal(1)
+        }
+      })
+      if (Val > -1) {
+        return null
+      } else {
+        return currencyFormatter(line_subtotal / quantity, currency)
+      }
+    }
+
+  }
 
   return (
     <ThemeConsumer>
@@ -72,8 +94,8 @@ function CartItem(props) {
               {Object.values(item.variation).map((data, index) =>
 
                 <Text style={{ color: 'black', fontSize: 11 }}>
-                    {/*{ data.indexOf('lb')<0 ? data.replace(/-/g,' ').replace('2 00','$2').replace('6','6"').replace('12','12"') : null }*/}
-                    { data.replace(/-/g,' ').replace('2 00','$2').replace('6','6"').replace('12','12"').replace('_','/') }
+                  {/*{ data.indexOf('lb')<0 ? data.replace(/-/g,' ').replace('2 00','$2').replace('6','6"').replace('12','12"') : null }*/}
+                  {data.replace(/-/g, ' ').replace('2 00', '$2').replace('6', '6"').replace('12', '12"').replace('_', '/')}
                 </Text>
 
               )}
@@ -82,9 +104,9 @@ function CartItem(props) {
               {item.veggie == null || item.veggie == undefined ?
                 null
                 :
-                <View style={{marginTop:10}}>
+                <View style={{ marginTop: 10 }}>
                   <Text style={{ fontSize: 14, fontWeight: 'bold' }}>Veggies</Text>
-                  {Object.values(item.veggie).map((data,index) => 
+                  {Object.values(item.veggie).map((data, index) =>
                     <Text style={{ color: 'black', fontSize: 11 }}>
                       {data.title}
                     </Text>
@@ -95,9 +117,9 @@ function CartItem(props) {
               {item.condi == null || item.condi == undefined ?
                 null
                 :
-                <View style={{marginTop:10}}>
+                <View style={{ marginTop: 10 }}>
                   <Text style={{ fontSize: 14, fontWeight: 'bold' }}>Condiments</Text>
-                  {Object.values(item.condi).map((data,index) => 
+                  {Object.values(item.condi).map((data, index) =>
                     <Text style={{ color: 'black', fontSize: 11 }}>
                       {data.title}
                     </Text>
@@ -107,7 +129,7 @@ function CartItem(props) {
             </View>
             <Quantity style={{ marginTop: 11 }} value={quantity} onChange={(value) => updateQuantity(key, value)} />
           </Col>
-          <Text medium>{currencyFormatter(line_subtotal / quantity, currency)}</Text>
+          <Text medium>{HideTotal(line_subtotal, quantity, currency, item)}</Text>
         </Row>
       )}
     </ThemeConsumer>
